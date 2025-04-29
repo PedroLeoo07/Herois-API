d1940a2ca97bfd7c9f66789291c5b2da19bcb191
Leonardo Oliveira <leonardodeoliveira.tds1.senai@gmail.com>
2025-04-29 08:39:17 -0300

    Feat: criação da tabela e dos inserts

---
+++ src/database/schema.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE heroi;
+
+/c heroi;
+
+CREATE TABLE herois (
+    id SERIAL PRIMARY KEY,
+    id_editora INT NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    poder VARCHAR(255) NOT NULL,
+    FOREIGN KEY (id_editora) REFERENCES editoras(id)
+);
+
+CREATE TABLE editores (
+    id SERIAL PRIMARY KEY,
+    id_heroi INT NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    sexo VARCHAR(100) NOT NULL,
+    idade INT NOT NULL,
+    FOREIGN KEY (id_heroi) REFERENCES herois(id)
+);
+
+INSERT INTO herois (id_editora, nome, poder) VALUES
+(1, 'Superman', 'Super força'),
+(2, 'Batman', 'Inteligência'),
+(3, 'Flash', 'Velocidade'),
+(4, 'Aquaman', 'Comunicação com animais marinhos'),
+(5, 'Mulher Maravilha', 'Força sobre-humana');
+
+INSERT INTO editores (id_heroi, nome, sexo, idade) VALUES
+(1, 'Clark Kent', 'Masculino', 35),
+(2, 'Bruce Wayne', 'Masculino', 40),
+(3, 'Barry Allen', 'Masculino', 30),
+(4, 'Arthur Curry', 'Masculino', 32),
+(5, 'Diana Prince', 'Feminino', 28);
