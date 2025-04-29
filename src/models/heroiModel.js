c8e8d0c75756a5f7fccce08dc9ba273f4fd280fd
Leonardo Oliveira <leonardodeoliveira.tds1.senai@gmail.com>
2025-04-29 09:10:51 -0300

    Feat: criação dos métodos Post, Put e delete no models

--- src/models/heroiModel.js
+++ src/models/heroiModel.js
@@ -10,5 +10,27 @@ const getHerois = async (description) => {
 }
 };
 
+const getHeroisById = async (id) => {
+    const result = await pool.query('SELECT herois.* editores.nome AS editor FROM herois LEFT JOIN editores ON herois.id_editor = editores.id WHERE herois.id = $1', [id]);
+    return result.rows[0];
+};
 
-module.exports = { getHerois, }
+const createHeroi = async (nome, poder, photo) => {
+    const result = await pool.query('INSERT INTO herois (nome, poder, photo) VALUES ($1, $2, $3) RETURNING *', [nome, poder, photo]);
+    return result.rows[0];
+}
+
+const updateHeroi = async (id, nome, poder) => {
+    const result = await pool.query('UPDATE herois SET nome = $1, poder = $2 WHERE id = $3 RETURNING *', [nome, poder, id]);
+    return result.rows[0];
+};
+
+const deleteHeroi = async (id) => {
+    const result = await pool.query('DELETE FROM herois WHERE id = $1 RETURNING *', [id]);
+   if (result.rowCount === 0) {
+        throw new Error('Heroi não encontrado');
+    }
+    return result.rows[0];
+}
+
+module.exports = { getHerois, getHeroisById, createHeroi, updateHeroi, deleteHeroi };
